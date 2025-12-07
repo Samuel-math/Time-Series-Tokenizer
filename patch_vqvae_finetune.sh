b037d96284189496862ae34921a51fc2d308036d
qqbondzhao <qqbondzhao@tencent.com>
2025-12-08 02:26:23 +0800

    feat: 更新预训练架构

--- patch_vqvae_finetune.sh
+++ patch_vqvae_finetune.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Patch VQVAE Transformer 微调脚本
+# 默认冻结 encoder 和 VQ 层，只训练 Transformer 和 Decoder
 
 # 预训练模型路径 (根据实际情况修改)
 PRETRAINED_MODEL="saved_models/patch_vqvae/ettm1/patch_vqvae_ps16_cb256_d128_l4_model1.pth"
@@ -11,8 +12,6 @@ python patch_vqvae_finetune.py \
     --target_points 96 \
     --batch_size 128 \
     --pretrained_model $PRETRAINED_MODEL \
-    --freeze_encoder 0 \
-    --freeze_transformer 0 \
     --n_epochs 30 \
     --lr 1e-4 \
     --weight_decay 1e-4 \
